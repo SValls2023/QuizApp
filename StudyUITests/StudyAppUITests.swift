aaac5400ce9cd085d49a3613fb9db9d2f0344797
SValls2023 <samv2019@csu.fullerton.edu>
2022-12-16 11:14:44 -0800

    Rename to fit StudyApp

--- StudyUITests/StudyAppUITests.swift
+++ StudyUITests/StudyAppUITests.swift
@@ -7,7 +7,7 @@
 
 import XCTest
 
-final class QuizAppUITests: XCTestCase {
+final class StudyAppUITests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
--- StudyUITests/StudyAppUITestsLaunchTests.swift
+++ StudyUITests/StudyAppUITestsLaunchTests.swift
@@ -7,7 +7,7 @@
 
 import XCTest
 
-final class QuizAppUITestsLaunchTests: XCTestCase {
+final class StudyAppUITestsLaunchTests: XCTestCase {
 
     override class var runsForEachTargetApplicationUIConfiguration: Bool {
         true
